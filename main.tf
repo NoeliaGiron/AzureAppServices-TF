dd2a16ab90fafc498afc154a0d30be4d31b97fca
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 22:29:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -8,23 +6,59 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.7.2"
+      version = "~> 3.7"
     }
   }
+
+  required_version = ">= 1.0"
 }
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
-# Generar nombre aleatorio para el resource group
-resource "random_pet" "rg_name" {
-  length    = 2
-  separator = "-"
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
-# Crear el Resource Group en Azure
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
+resource "azurerm_resource_group" "app_rg" {
+  name     = "rg-tf-appservice"
   location = "East US"
+}
+
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "appservice-plan"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "web_app" {
+  name                = "tf-webapp-example-${random_string.suffix.result}"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+
+  site_config {
+    always_on = false
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
+
+output "app_service_url" {
+  value = azurerm_app_service.web_app.default_site_hostname
 }
