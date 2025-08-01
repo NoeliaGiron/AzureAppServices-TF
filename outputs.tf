9220eb6db8cfaf04e50a16b49e81488d6910d802
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 23:06:28 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "app_service_url" {
-  value = azurerm_windows_web_app.web_app.default_hostname
+output "app_service_default_hostname" {
+  description = "URL del App Service"
+  value       = azurerm_windows_web_app.web_app.default_site_hostname
 }
