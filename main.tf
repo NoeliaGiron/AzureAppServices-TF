aa34e83c91df2ed950b90d55d8e046b18f7f694b
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 22:49:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,13 +15,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # No es necesario poner aquí subscription_id, tenant_id, client_id, client_secret
+  # porque Jenkins ya exporta esas variables de entorno y el provider las detecta automáticamente
 }
-
 
 resource "random_pet" "name" {
   length    = 2
@@ -38,15 +34,15 @@ resource "azurerm_service_plan" "app_plan" {
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
 
-  sku_name = "F1"      # Plan Free, puedes cambiar por S1, P1v2, etc. según necesidad
+  sku_name = "F1"      # Plan Free, puedes cambiar por S1, P1v2, etc.
   os_type  = "Windows" # Sistema operativo Windows
 }
 
-resource "azurerm_app_service" "web_app" {
+resource "azurerm_windows_web_app" "web_app" {
   name                = "webapp-${random_pet.name.id}"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
-  app_service_plan_id = azurerm_service_plan.app_plan.id
+  service_plan_id     = azurerm_service_plan.app_plan.id
 
   site_config {
     always_on = false
