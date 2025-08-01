7ada9adffc7ca2c9dc45bb65968946686f3c72e2
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 22:07:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,3 @@ resource "azurerm_linux_web_app" "web_app" {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 }
-
-output "app_service_url" {
-  value = azurerm_linux_web_app.web_app.default_site_hostname
-}
