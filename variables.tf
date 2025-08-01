e1871535957057af23ac8a6a3eaf60767120f5be
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 23:06:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,44 @@
 variable "subscription_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
+  description = "ID de la suscripción de Azure"
+  type        = string
 }
 
 variable "client_id" {
-  type = string
+  description = "Client ID del service principal"
+  type        = string
 }
 
 variable "client_secret" {
-  type = string
+  description = "Client secret del service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Tenant ID de Azure"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Nombre del Resource Group"
+  type        = string
+  default     = "rg-terraform-example"
+}
+
+variable "location" {
+  description = "Ubicación para los recursos"
+  type        = string
+  default     = "East US"
+}
+
+variable "app_service_plan_name" {
+  description = "Nombre del App Service Plan"
+  type        = string
+  default     = "plan-appservice-example"
+}
+
+variable "app_service_name" {
+  description = "Nombre del App Service"
+  type        = string
+  default     = "appservice-example"
 }
