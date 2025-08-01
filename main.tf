b14221db6c2c5bd0793fe2c704a7e896b3644851
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 22:40:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.38.1"
+      version = "~> 4.38"
     }
     random = {
       source  = "hashicorp/random"
@@ -10,45 +10,37 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  upper   = false
-  special = false
+resource "random_pet" "name" {
+  length    = 2
+  separator = "-"
 }
 
 resource "azurerm_resource_group" "app_rg" {
-  name     = "rg-tf-appservice"
+  name     = "rg-${random_pet.name.id}"
   location = "East US"
 }
 
 resource "azurerm_service_plan" "app_plan" {
-  name                = "appservice-plan"
+  name                = "plan-${random_pet.name.id}"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
 
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  sku_name = "F1"      # Plan Free, puedes cambiar por S1, P1v2, etc. según necesidad
+  os_type  = "Windows" # Sistema operativo Windows
 }
 
 resource "azurerm_app_service" "web_app" {
-  name                = "tf-webapp-example-${random_string.suffix.result}"
+  name                = "webapp-${random_pet.name.id}"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+  app_service_plan_id = azurerm_service_plan.app_plan.id
 
   site_config {
     always_on = false
