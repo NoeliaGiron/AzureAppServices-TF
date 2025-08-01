eda92fc80923703385ce2b450ecabce89f50b893
NoeliaGiron <133072524+NoeliaGiron@users.noreply.github.com>
2025-07-31 21:56:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "azurerm_app_service_plan" "app_plan" {
   name                = "appservice-plan"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
+
   sku {
     tier = "Free"
     size = "F1"
@@ -23,7 +24,7 @@ resource "azurerm_app_service_plan" "app_plan" {
 }
 
 resource "azurerm_app_service" "web_app" {
-  name                = "tf-webapp-example"
+  name                = "neoliaapp"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
   app_service_plan_id = azurerm_app_service_plan.app_plan.id
